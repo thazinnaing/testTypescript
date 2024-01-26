0f66decf0ce00320afe8a8a7ce153aeeead73deb
Thazin <thazinnaingmm@gmail.com>
2024-01-26 12:53:17 +0630

    feat: testing ts

--- src/App.tsx
+++ src/App.tsx
@@ -79,6 +79,45 @@ const App = () => {
   const objArr : ObjectArray = [{name:"Thazin",id:1},{name:"Naing",id:2}]
   console.log(objArr)
 
+  const greet=(person:string, date:Date)=>{
+    console.log("Hello ".concat(person, ", Today is").concat(date.toDateString(), "!"))
+    console.log(`Hi ${person}, Today is ${date.toDateString()}! A good day. `)
+  }
+  greet("Hazal", new Date())
+
+  let myName="Myat Myat"
+  console.log(typeof(myName))
+  myName="MYO"
+
+  const getFavouriteNumber=():number=>{
+    return 23
+  }
+  console.log(getFavouriteNumber());
+
+  const addTwoNumbers=(a:number, b:number):string=>{
+    return (`Add two numbers, ${a} and ${b} is ${a+b}`)
+  }
+
+  console.log(addTwoNumbers(2,4))
+
+  const asyncFun=async():Promise<{name:string, id:number}>=>{
+    return {name:"Hla", id:2};
+  }
+  console.log(asyncFun())
+
+   //contextual typing because the context that the function within informs what type it should have
+  const names=["Alice", "Bob", "Eve"]
+
+  names.forEach((name)=>{
+    console.log(name.toUpperCase())
+  })
+
+  
+
+
+
+  
+
   return (
     <div>
       Hello
